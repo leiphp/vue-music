c05d837b372361270aeb17993c4a85c83581a5ef
leiwen <657106593@qq.com>
2019-11-10 23:01:29 +0800

    :heart:完善路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,90 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import Recommend from 'components/recommend/recommend'
+// import Singer from 'components/singer/singer'
+// import Rank from 'components/rank/rank'
+// import Search from 'components/search/search'
+//
+// Vue.use(Router)
+//
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       redirect: '/recommend'
+//     },
+//     {
+//       path: '/recommend',
+//       component: Recommend
+//     },
+//     {
+//       path: '/singer',
+//       component: Singer
+//     },
+//     {
+//       path: '/rank',
+//       component: Rank
+//     },
+//     {
+//       path: '/search',
+//       component: Search
+//     }
+//   ]
+// })
+
+
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from 'components/recommend/recommend'
-import Singer from 'components/singer/singer'
-import Rank from 'components/rank/rank'
-import Search from 'components/search/search'
 
 Vue.use(Router)
+
+const Recommend = (resolve) => {
+  import('components/recommend/recommend').then((module) => {
+    resolve(module)
+  })
+}
+
+const Singer = (resolve) => {
+  import('components/singer/singer').then((module) => {
+    resolve(module)
+  })
+}
+
+const Rank = (resolve) => {
+  import('components/rank/rank').then((module) => {
+    resolve(module)
+  })
+}
+
+const Search = (resolve) => {
+  import('components/search/search').then((module) => {
+    resolve(module)
+  })
+}
+
+const SingerDetail = (resolve) => {
+  import('components/singer-detail/singer-detail').then((module) => {
+    resolve(module)
+  })
+}
+
+const Disc = (resolve) => {
+  import('components/disc/disc').then((module) => {
+    resolve(module)
+  })
+}
+
+const TopList = (resolve) => {
+  import('components/top-list/top-list').then((module) => {
+    resolve(module)
+  })
+}
+
+const UserCenter = (resolve) => {
+  import('components/user-center/user-center').then((module) => {
+    resolve(module)
+  })
+}
 
 export default new Router({
   routes: [
@@ -15,19 +94,47 @@ export default new Router({
     },
     {
       path: '/recommend',
-      component: Recommend
+      component: Recommend,
+      children: [
+        {
+          path: ':id',
+          component: Disc
+        }
+      ]
     },
     {
       path: '/singer',
-      component: Singer
+      component: Singer,
+      children: [
+        {
+          path: ':id',
+          component: SingerDetail
+        }
+      ]
     },
     {
       path: '/rank',
-      component: Rank
+      component: Rank,
+      children: [
+        {
+          path: ':id',
+          component: TopList
+        }
+      ]
     },
     {
       path: '/search',
-      component: Search
+      component: Search,
+      children: [
+        {
+          path: ':id',
+          component: SingerDetail
+        }
+      ]
+    },
+    {
+      path: '/user',
+      component: UserCenter
     }
   ]
 })
