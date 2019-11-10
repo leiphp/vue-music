07263cb6ca84cf4e260bfd2deb3d97f78a849e60
leiwen <657106593@qq.com>
2019-11-10 23:13:15 +0800

    :heart:完善路由

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
-  <div id="app">
+  <div id="app" @touchmove.prevent>
     <m-header></m-header>
     <tab></tab>
-    <router-view></router-view>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+    <player></player>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import MHeader from 'components/m-header/m-header'
-import Tab from 'components/tab/tab'
-export default {
-  components: {
-    MHeader,
-    Tab
+  import MHeader from 'components/m-header/m-header'
+  import Player from 'components/player/player'
+  import Tab from 'components/tab/tab'
+
+  export default {
+    components: {
+      MHeader,
+      Tab,
+      Player
+    }
   }
-}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
 </style>
